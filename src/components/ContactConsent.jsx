fedab62992b78f2cec3a31bcd1fc592d4a44ef36
Steno-95 <lezzistefano95@gmail.com>
2025-05-12 12:47:03 +0200

    Feat:add error functionality and layout

--- src/components/ContactConsent.jsx
+++ src/components/ContactConsent.jsx
@@ -1,14 +1,25 @@
-function ContactConsent() {
+function ContactConsent({ error, onError }) {
   return (
     <fieldset>
-      <input
-        type="checkbox"
-        id="consent"
-        name="consent-auth"
-        aria-autocomplete="false"
-      />
-      <label htmlFor="consent">I consent to being contacted by the team</label>
-      <span>To submit this form, please consent to being contacted</span>
+      <div className="flex items-center gap-4 ">
+        <input
+          type="checkbox"
+          id="consent"
+          name="consentAuth"
+          aria-autocomplete="false"
+          className="accent-(--primary-green) cursor-pointer peer"
+          onChange={() => onError({ ...error, consentAuth: false })}
+        />
+        <label
+          htmlFor="consent"
+          className="cursor-pointer peer-focus:text-(--primary-green) font-normal"
+        >
+          I consent to being contacted by the team
+        </label>
+      </div>
+      <span className={`err-msg ${error.consentAuth ? "show" : ""}`}>
+        To submit this form, please consent to being contacted
+      </span>
     </fieldset>
   );
 }
