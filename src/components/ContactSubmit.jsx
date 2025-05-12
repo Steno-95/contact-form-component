95e8ac274c5b6013c860ba7da32642ad3706abcb
Steno-95 <lezzistefano95@gmail.com>
2025-05-12 12:45:18 +0200

    Refactor: move btn functionality to a component

---
+++ src/components/ContactSubmit.jsx
@@ -0,0 +1,12 @@
+function ContactSubmit() {
+  return (
+    <button
+      className="bg-(--primary-green) text-(--white) py-2 rounded-lg"
+      type="submit"
+    >
+      Submit
+    </button>
+  );
+}
+
+export default ContactSubmit;
